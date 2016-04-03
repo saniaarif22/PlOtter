84583f7613d7d77cbe1ae7f4a462f07a64dbe553
Sania Arif <sania.arif@columbia.edu>
2016-04-03 19:15:55 -0400

    Simpler hello world

--- C++_sample/helloworld.cpp
+++ C++_sample/helloworld.cpp
@@ -5,14 +5,6 @@ using namespace std;
 
 ofstream f;
 
-// Prolog for the SVG image
-
-void header() {
-  string header = "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"> width=\"100\" height=\"30\"";
-  f << header;
-  f << "\n";
-}
-
 // SVG content
 void put_in_svg(std::string content) 
 {
@@ -25,9 +17,13 @@ void put_in_svg(std::string content)
 int main() {
   f.open ("myFile.svg");
   string content = "<rect x=\"20\" y=\"20\" width=\"50\" height=\"20\" style=\"stroke: none; fill: red;\" />";
-
-  header();
+  
+  // Prolog for the SVG image
+  f << "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"> width=\"100\" height=\"30\"";
+  f << "\n";
+  
   put_in_svg(content);
+  
   // Epilog for the SVG image
   f << "</svg>";
   f << "\n";
