95e29ac1ad9dab683b2282202e38f5820900a0d4
Sania Arif <sania.arif@columbia.edu>
2016-05-04 15:16:57 -0400

    merging

--- src/sast.ml
+++ src/sast.ml
@@ -1,34 +1,35 @@
 
 open Ast
 
-type t =  Num | Bool | String | Point
+type t =  Num | Bool | String | Point | List
         | ListNum | ListString | ListPoint | ListBool
 
 type texpr =
-			Literal_Num of float * t               
-  			| Literal_Str of string * t
-  			| Binop of texpr * Ast.ops * texpr * t
-  			| Id of string * t
-  			| Bool of bool * t
+      Literal_Num of float * t
+        | Literal_Str of string * t
+            | Literal_List of texpr list * t
+        | Binop of texpr * Ast.ops * texpr * t
+        | Id of string * t
+        | Bool of bool * t
 
 type tstmt =
     Expr of texpr * t
   | Var_Decl of string * string * t
-  | List_Decl of string * string *  t         
-  | Passign of texpr * texpr * texpr             
-  | Assign of texpr * texpr             
+  | List_Decl of string * string *  t
+  | Passign of texpr * texpr * texpr
+  | Assign of texpr * texpr
   | Print of texpr
   | LineVar of texpr * texpr
   | LineRaw of texpr * texpr * texpr * texpr
   | For of tstmt * texpr * tstmt * tstmt list
-  | While of texpr * tstmt list                      
+  | While of texpr * tstmt list
   | Return of texpr
 
 type program = tstmt list
 
 (* Pretty Print Stuff *)
 
-let typeof t = 
+let typeof t =
   match t with
         | Num -> "num"
         | Bool -> "bool"
@@ -38,21 +39,23 @@ let typeof t =
         | ListString -> "listString"
         | ListBool   -> "listBool"
         | ListPoint  -> "listPoint"
+        | List -> "list"
 
 
 let rec string_of_texpr = function
     Literal_Num(l, t) -> string_of_float l ^ typeof t
   | Literal_Str(l, t) -> l ^ typeof t
+  | Literal_List(l, t) ->  typeof t
   | Id(s, t) -> s ^ typeof t
   | Binop(e1, o, e2, t) ->
       string_of_texpr e1 ^ " " ^
       (match o with
   Add -> "+" | Sub -> "-" | Mul -> "*" | Div -> "/"
       | Equal -> "==" | Neq -> "!="
-      | Mod -> "%" 
+      | Mod -> "%"
       | And -> "&&" | Or ->"||"
       | Square -> "**"
-      | Less -> "<" | Leq -> "<=" 
+      | Less -> "<" | Leq -> "<="
       | Greater -> ">" | Geq -> ">="
       ) ^ " " ^ string_of_texpr e2 ^ typeof t
   | Bool(x, t) -> if x = True then "true" else "false" ^ typeof t
@@ -64,11 +67,11 @@ let rec string_of_tstmt = function
   | List_Decl(tp, id, t) -> "list" ^ tp ^ " " ^ id ^ "\n" ^ typeof t
   | Passign(v, e1, e2) -> string_of_texpr v ^ " = (" ^ ( string_of_texpr e1 ) ^ "," ^ ( string_of_texpr e2 ) ^ ")\n"
   | Assign(v, e) -> string_of_texpr v ^ " = " ^ ( string_of_texpr e )
-  | Print(e) -> "print " ^ string_of_texpr e ^ "\n" 
-  | LineVar(e1,e2)-> "line (" ^ string_of_texpr e1 ^ "," ^ string_of_texpr e2 ^ ")" ^ "\n" 
-  | LineRaw(e1,e2,e3,e4)-> "line ( (" ^ string_of_texpr e1 ^ "," ^ string_of_texpr e2 ^ ")" ^ "," ^ "(" 
-                            ^ string_of_texpr e3 ^ "," ^ string_of_texpr e4 ^ ") )\n" 
-  | For(s1, e1, s2, body) -> "for " ^ string_of_tstmt s1 ^ " ; " ^ string_of_texpr e1 ^ " ; " ^ string_of_tstmt s2 ^ ": \n" 
+  | Print(e) -> "print " ^ string_of_texpr e ^ "\n"
+  | LineVar(e1,e2)-> "line (" ^ string_of_texpr e1 ^ "," ^ string_of_texpr e2 ^ ")" ^ "\n"
+  | LineRaw(e1,e2,e3,e4)-> "line ( (" ^ string_of_texpr e1 ^ "," ^ string_of_texpr e2 ^ ")" ^ "," ^ "("
+                            ^ string_of_texpr e3 ^ "," ^ string_of_texpr e4 ^ ") )\n"
+  | For(s1, e1, s2, body) -> "for " ^ string_of_tstmt s1 ^ " ; " ^ string_of_texpr e1 ^ " ; " ^ string_of_tstmt s2 ^ ": \n"
                             ^ ( String.concat "\n\t" (List.map string_of_tstmt body) )
                             ^ "\nend\n"
   | While(e, body) -> "while " ^ string_of_texpr e ^ " :\n" ^ (String.concat "\n\t" (List.map string_of_tstmt body)) ^ "\nend\n"
