513d6bff9d2832578e7a6e79832d5d97e36a7f08
Ibrahima Niang <in2190@columbia.edu>
2016-05-06 18:45:37 -0400

    Errors removed

--- src/cppsast.ml
+++ src/cppsast.ml
@@ -3,38 +3,48 @@ open Tast
 open Ast
 
 (* Convert ast expr to Tast expr *)
-
+let match_of e = match e with
+| Ast.Add -> Tast.Add
+| Ast.Sub -> Tast.Sub
+| Ast.Mul -> Tast.Mul
+| Ast.Div -> Tast.Div
+| Ast.Mod -> Tast.Mod
+| Ast.Equal -> Tast.Equal
+| Ast.Neq -> Tast.Neq
+| Ast.Add -> Tast.Add
+| Ast.Less -> Tast.Less
+| Ast.Leq -> Tast.Leq
+| Ast.Greater -> Tast.Greater
+| Ast.Geq -> Tast.Geq
+| Ast.And -> Tast.And
+| Ast.Or -> Tast.Or
+| Ast.Square -> Tast.Square
 
 let convert_to_cppast stmts_list =
-let rec convert_to_texpr stmt = function
-  | Ast.Literal_Num(v) -> Tast.Literal_Float(v)
-  | Ast.Literal_Str(v) -> Tast.Literal_Str(v)
-  | Ast.Bool(v)        -> if v = True then Tast.Bool(True) else Tast.Bool(False)
-  | Ast.Binop(e1, op, e2) ->
-      let te1 = convert_to_texpr stmt e1 in
-      let te2 = convert_to_texpr stmt e2 in
-      Tast.Binop(te1, op, te2)
-  | Ast.Id(v)          -> Tast.Id(v)
+  let rec convert_to_texpr  = function
+    | Ast.Literal_Num(v) -> Tast.Literal_Float(v)
+    | Ast.Literal_Str(v) -> Tast.Literal_Str(v)
+    | Ast.Bool(v)        -> if v = True then Tast.Bool(True) else Tast.Bool(False)
+    | Ast.Binop(e1, op, e2) ->
+        let te1 = convert_to_texpr e1 in
+        let te2 = convert_to_texpr e2 in
+        let top = match_of op in
+        Tast.Binop(te1, top, te2)
+    | Ast.Id(v)          -> Tast.Id(v)
 
   in
 
-(* Convert ast stmt to Tast stmt *)
-let rec convert_to_tstmt  = function
-  | Ast.Expr(e) ->
-      let te = convert_to_texpr e in
-      Tast.Expr(te)
-  | Ast.Passign(v,e1,e2) ->
+  (* Convert ast stmt to Tast stmt *)
+  let rec convert_to_tstmt stmt = function
+    | Ast.Expr(e) ->
+        let te = convert_to_texpr e in
+        Tast.Expr(te)
+    | Ast.Assign(e1,e2) ->
       let te1 = convert_to_texpr e1 in
       let te2 = convert_to_texpr e2 in
-    Sast.Passign(sv, se1, se2)
-  | Ast.Assign(e1,e2) ->
-    let te1 = convert_to_texpr e1 in
-    let te2 = convert_to_texpr e2 in
-      Sast.Assign(te1, te1)
-  | Ast.Print(e) -> Sast.Print(convert_to_texpr e)
-  | Ast.Return(e) -> Sast.Return(convert_to_texpr e)
+        Tast.Assign(te1, te1)
+    | Ast.Print(e) -> Tast.Print(convert_to_texpr e)
+    | Ast.Return(e) -> Tast.Return(convert_to_texpr e)
 
   in
   List.map (fun s -> convert_to_tstmt s) stmts_list
-in
-Tast.string_of_tprogram (convert_to_cppast stmt)
