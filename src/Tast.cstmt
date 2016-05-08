c3302920b81d745ab5fc080856f9c590a0aa7a4b
Ibrahima Niang <in2190@columbia.edu>
2016-05-08 19:57:59 -0400

    Tast and cppsast updated

---
+++ src/Tast.cstmt
@@ -0,0 +1,1 @@
+type is a shell builtin
--- src/plotter.ml
+++ src/plotter.ml
@@ -1,13 +1,14 @@
 
-type action = Ast | Codegen
+type action = Ast | Codegen | Tast | Cppsast
 
 let _ =
   let action = if Array.length Sys.argv > 1 then
     List.assoc Sys.argv.(1) [ ("-a", Ast);
-                ("-c", Codegen) ]
+                ("-c", Codegen); ("-t", Tast) ]
   else Codegen in
   let lexbuf = Lexing.from_channel stdin in
   let program = Parser.program Scanner.token lexbuf in
   match action with
     Ast -> print_string (Ast.string_of_program (List.rev program))
   | Codegen -> print_string (Codegen.convert (List.rev program))
+  (*| Tast -> Cppsast.convert_to_cppast (List.rev program)*)
