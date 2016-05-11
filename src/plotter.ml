b0202242d3f717bac86e10829aa66aa42e366106
ranji2612 <ranji2612@gmail.com>
2016-05-11 03:59:36 -0400

    Removing warnings

--- src/plotter.ml
+++ src/plotter.ml
@@ -1,10 +1,10 @@
 
-type action = Ast | Codegen | Tast | Cppsast
+type action = Ast | Codegen (* | Tast | Cppsast *)
 
 let _ =
   let action = if Array.length Sys.argv > 1 then
     List.assoc Sys.argv.(1) [ ("-a", Ast);
-                ("-c", Codegen); ("-t", Tast) ]
+                ("-c", Codegen); (* ("-t", Tast) *) ]
   else Codegen in
   let lexbuf = Lexing.from_channel stdin in
   let program = Parser.program Scanner.token lexbuf in
